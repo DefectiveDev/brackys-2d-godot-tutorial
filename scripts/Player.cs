1f713be85119a54338b6ec62cacd341a16e6a486
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-06 04:49:19 -0500

    fliping character2d dependent on movement direction. swapped ui controls for action maps.

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -6,6 +6,13 @@ public partial class Player : CharacterBody2D
 	public const float Speed = 130.0f;
 	public const float JumpVelocity = -300.0f;
 
+
+    AnimatedSprite2D animatedSprite2D;
+	public override void _Ready()
+    {
+        animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
@@ -17,14 +24,24 @@ public partial class Player : CharacterBody2D
 		}
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("move_left", "move_right", "ui_up", "ui_down");
+
+        if (direction.X > 0)
+        {
+            animatedSprite2D.FlipH = false;
+        }
+        else if (direction.X < 0)
+        {
+            animatedSprite2D.FlipH = true;
+        }
+
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;

76859aa662e148cb6c1a22d0807d0cb07d6dda93
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-06 04:38:48 -0500

    disable player collsion on death and slow game down

--- scripts/KillZone.cs
+++ scripts/KillZone.cs
@@ -13,11 +13,14 @@ public partial class KillZone : Area2D
     private void OnBodyEntered(Node2D body)
     {
         GD.Print("You died!");
+        Engine.TimeScale = 0.5;
+        body.GetNode<CollisionShape2D>("CollisionShape2D").QueueFree();
         timer.Start();
     }
 
     private void OnTimerTimout()
     {
+        Engine.TimeScale = 1;
         GetTree().ReloadCurrentScene();
     }
 }

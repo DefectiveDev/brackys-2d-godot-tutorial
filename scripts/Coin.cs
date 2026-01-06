6411cbe6f469526d716bf9b31aa76fcfd577adfc
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-06 03:13:40 -0500

    basic coin remove and print for coin added. changed physics for player and coin so only layer 2, which is the same as the player causes collision

--- scripts/Coin.cs
+++ scripts/Coin.cs
@@ -3,13 +3,9 @@ using System;
 
 public partial class Coin : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    private void OnBodyEntered(Node2D body)
+    {
+        GD.Print("+1 coin!");
+        QueueFree();
+    }
 }

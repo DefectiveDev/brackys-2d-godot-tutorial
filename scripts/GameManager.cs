a00e60fe009241ab541fdf2c1acd8dc33768de1d
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-06 05:24:52 -0500

    score updates label at end.

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -4,10 +4,16 @@ using System;
 public partial class GameManager : Node
 {
     private int score = 0;
+    private Label scoreLabel;
+
+    public override void _Ready()
+    {
+        scoreLabel = GetNode<Label>("ScoreLabel");
+    }
 
     public void AddPoint()
     {
         score += 1;
-        GD.Print(score);
+        scoreLabel.Text = $"You collected {score} coins.";
     }
 }

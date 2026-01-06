6fe165705b728143324fc44497629fed2395db4a
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-06 03:32:25 -0500

    set up killzone reload for scene

--- scripts/KillZone.cs
+++ scripts/KillZone.cs
@@ -3,4 +3,21 @@ using System;
 
 public partial class KillZone : Area2D
 {
+    Timer timer;
+
+    public override void _Ready()
+    {
+        timer = GetNode<Timer>("Timer");
+    }
+
+    private void OnBodyEntered(Node2D body)
+    {
+        GD.Print("You died!");
+        timer.Start();
+    }
+
+    private void OnTimerTimout()
+    {
+        GetTree().ReloadCurrentScene();
+    }
 }
